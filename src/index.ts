bc9a63eb791bf61d1a0347b4ed403dd4bb5f47c6
Tony Aldridge <tony@dabapps.com>
2017-10-20 11:02:44 +0100

    More actions, and tests

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,77 @@ export function RecordWithConstructor<T, U>(
     return constructorFunction(preTransform);
   };
 }
+
+export function recordOrUndefined<T, U>(
+  data: T | undefined,
+  constructorFunction: (input: T) => U
+): U | undefined {
+  if (data !== undefined) {
+    return constructorFunction(data);
+  }
+  return undefined;
+}
+
+export function recordOrNull<T, U>(
+  data: T | null,
+  constructorFunction: (input: T) => U
+): U | null {
+  if (data !== null) {
+    return constructorFunction(data);
+  }
+  return null;
+}
+
+export function recordOrId<T, U>(
+  data: T | string,
+  constructorFunction: (input: T) => U
+): U | string {
+  if (!_.isString(data)) {
+    return constructorFunction(data as T);
+  }
+  return data as string;
+}
+
+export function recordArray<T, U>(
+  data: ReadonlyArray<T> | undefined,
+  ctor: (item: T) => U
+): ReadonlyArray<U> {
+  if (!data) {
+    return [];
+  }
+  return _.map(data, (item) => ctor(item));
+}
+
+export function recordOrIdArray<T, U>(
+  data: ReadonlyArray<Partial<T>> | ReadonlyArray<string>,
+  constructor: (item: Partial<T>) => U
+): ReadonlyArray<U> | ReadonlyArray<string> {
+  const first = data[0];
+
+  if (_.isString(first)) {
+    return data as ReadonlyArray<string>;
+  } else {
+    return _.map(data as T[], (item) => constructor(item));
+  }
+}
+
+export function narrowToRecord<T>(input: T | string): T {
+  if (_.isString(input)) {
+    throw new Error(
+      `Tried to narrow an ID into a Record - ${typeof input} ${input}`
+    );
+  }
+  return input as T;
+}
+
+export function narrowToRecordArray<T>(input: ReadonlyArray<T> | ReadonlyArray<string>): ReadonlyArray<T> {
+  if (input.length === 0) {
+    return [];
+  }
+  const first = input[0];
+
+  if (_.isString(first)) {
+    throw new Error('Tried to narrow an Array of IDs');
+  }
+  return input as ReadonlyArray<T>;
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,14 @@
-import { RecordWithConstructor, SimpleRecord } from '../src/';
+import {
+  narrowToRecord,
+  narrowToRecordArray,
+  recordArray,
+  recordOrId,
+  recordOrIdArray,
+  recordOrNull,
+  recordOrUndefined,
+  RecordWithConstructor,
+  SimpleRecord,
+} from '../src/';
 
 type ISimpleInterface = Readonly<{
   a: number;
@@ -58,5 +68,89 @@ describe('RecordWithConstructor', () => {
         a: 10
       }
     });
+  });
+});
+
+describe('recordOrUndefined', () => {
+  it('should provide us with a constructed record if data is available', () => {
+    const item = recordOrUndefined({}, MySimpleRecord);
+    expect((item as any).a).toBe(5);
+  });
+
+  it('should provide us with undefined if no data is available', () => {
+    const item = recordOrUndefined(undefined, MySimpleRecord);
+    expect(item).toBe(undefined);
+  });
+});
+
+describe('recordOrNull', () => {
+  it('should provide us with a constructed record if data is available', () => {
+    const item = recordOrNull({}, MySimpleRecord);
+    expect((item as any).a).toBe(5);
+  });
+
+  it('should provide us with null if no data is available', () => {
+    const item = recordOrNull(null, MySimpleRecord);
+    expect(item).toBe(null);
+  });
+});
+
+describe('recordOrId', () => {
+  it('should provide us with a constructed record if data is available', () => {
+    const item = recordOrId({}, MySimpleRecord);
+    expect((item as any).a).toBe(5);
+  });
+
+  it('should provide us with a string if only a string is available', () => {
+    const item = recordOrId('12345', MySimpleRecord);
+    expect(item).toBe('12345');
+  });
+});
+
+describe('recordArray', () => {
+  it('should lift our Record constructor across an array', () => {
+    const items = recordArray([{}, {}], MySimpleRecord);
+    expect(items[0].a).toBe(5);
+    expect(items[1].a).toBe(5);
+  });
+});
+
+describe('recordOrIdArray', () => {
+  it('should provide us with a constructed record array if data is available', () => {
+    const items = recordOrIdArray([{}, {}], MySimpleRecord);
+    expect((items[0] as any).a).toBe(5);
+    expect((items[1] as any).a).toBe(5);
+  });
+
+  it('should provide us with a string array if only a string is available', () => {
+    const items = recordOrIdArray(['1', '2'], MySimpleRecord);
+    expect(items[0]).toBe('1');
+    expect(items[1]).toBe('2');
+  });
+});
+
+describe('narrowToRecord', () => {
+  it('should give us a record if one is available', () => {
+    const item = narrowToRecord(MySimpleRecord());
+    expect(item.a).toBe(5);
+  });
+
+  it('should throw if no record is available', () => {
+    expect(() => {
+      const item = narrowToRecord('5');
+    }).toThrow();
+  });
+});
+
+describe('narrowToRecordArray', () => {
+  it('should give us a record array if one is available', () => {
+    const items = narrowToRecordArray([MySimpleRecord()]);
+    expect(items[0].a).toBe(5);
+  });
+
+  it('should throw if no record array is available', () => {
+    expect(() => {
+      const items = narrowToRecordArray(['5', '6']);
+    }).toThrow();
   });
 });
