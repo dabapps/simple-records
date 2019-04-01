3a2ccbea15dff629c5f6f683e0937167e7838f2a
Tony Aldridge <tony@dabapps.com>
2019-04-01 15:19:37 +0100

    Corrected JSON type definition

--- src/core.ts
+++ src/core.ts
@@ -6,7 +6,7 @@ export type Dict<T> = Readonly<{ [key: string]: T }>;
 export type Pairs<T, U> = ReadonlyArray<[T, U]>;
 export type MutablePairs<T, U> = Array<[T, U]>;
 
-export type JSON = string | number | boolean | JSONObject | JSONArray;
+export type JSON = string | number | boolean | null | JSONObject | JSONArray;
 export interface JSONObject {
   [x: string]: JSON;
 }
@@ -16,6 +16,7 @@ export type ReadonlyJSON =
   | string
   | number
   | boolean
+  | null
   | ReadonlyJSONObject
   | ReadonlyJSONArray;
 export interface ReadonlyJSONObject {
