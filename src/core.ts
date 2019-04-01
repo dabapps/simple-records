29cf0052d751153896977ad194ae8ec5230d5e6c
Tony Aldridge <tony@dabapps.com>
2019-04-01 15:17:21 +0100

    Added JSON types

--- src/core.ts
+++ src/core.ts
@@ -5,3 +5,20 @@ export interface ObjectKeys {
 export type Dict<T> = Readonly<{ [key: string]: T }>;
 export type Pairs<T, U> = ReadonlyArray<[T, U]>;
 export type MutablePairs<T, U> = Array<[T, U]>;
+
+export type JSON = string | number | boolean | JSONObject | JSONArray;
+export interface JSONObject {
+  [x: string]: JSON;
+}
+export interface JSONArray extends Array<JSON> {}
+
+export type ReadonlyJSON =
+  | string
+  | number
+  | boolean
+  | ReadonlyJSONObject
+  | ReadonlyJSONArray;
+export interface ReadonlyJSONObject {
+  readonly [x: string]: ReadonlyJSON;
+}
+export interface ReadonlyJSONArray extends ReadonlyArray<ReadonlyJSON> {}
